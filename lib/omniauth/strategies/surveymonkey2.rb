e11c0d3e9cabf8b52063763d1906fa160f421e65
Chris Beck <chris.beck@me.com>
2013-12-04 09:49:26 -0500

    added custom code

--- lib/omniauth/strategies/surveymonkey2.rb
+++ lib/omniauth/strategies/surveymonkey2.rb
@@ -4,21 +4,54 @@ module OmniAuth
   module Strategies
     class Surveymonkey2 < OmniAuth::Strategies::OAuth2
 
+      DEFAULT_RESPONSE_TYPE = 'code'
+      DEFAULT_GRANT = 'authorization_code'
+
       option :name, "surveymonkey2"
 
       option :client_options, {
-        :site => "https://api.surveymonkey.com",
+        :site => "https://api-surveymonkey-com-fytofsd4ktc2.runscope.net",
         :authorize_url => '/oauth/authorize',
         :token_url => '/oauth/token'
       }
 
-      option :authorize_options, [:api_key]
+      option :authorize_options, [:client_id, :api_key]
+
+      def authorize_params
+        super.tap do |params|
+          params[:response_type] ||= DEFAULT_RESPONSE_TYPE
+          params[:client_id] = options[:client_id]
+          params[:api_key] = options[:api_key]
+        end
+      end
+
+      def token_params
+        super.tap do |params|
+          params[:grant_type] ||= DEFAULT_GRANT
+          params[:client_id] = options[:client_id]
+          params[:client_secret] = options[:client_secret]
+          params[:redirect_uri] = callback_url
+        end
+      end
+
+      def build_access_token
+        verifier = request.params['code']
+        token = client.auth_code.get_token(verifier, token_params)
+      end
       
       def callback_phase
         options[:client_options][:token_url] = "/oauth/token?api_key=#{options[:api_key]}"
-        super
+        self.access_token = build_access_token
+        self.env['omniauth.auth'] = auth_hash
+        call_app!
       end
 
+      info do
+        {
+          :token => self.access_token.token,
+          :client => self.access_token.client.id
+        }
+      end
     end
   end
 end
--- lib/omniauth/surveymonkey2/version.rb
+++ lib/omniauth/surveymonkey2/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Surveymonkey2
-    VERSION = "0.0.10"
+    VERSION = "0.0.11"
   end
 end
